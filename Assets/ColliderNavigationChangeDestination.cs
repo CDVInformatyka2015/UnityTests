8ea27a0d353b08023b524040a5c7bf84660211f8
otlet <otlet@otlet.pl>
2018-06-24 15:51:57 +0200

    HueHueHue

--- Assets/ColliderNavigationChangeDestination.cs
+++ Assets/ColliderNavigationChangeDestination.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 
 public class ColliderNavigationChangeDestination : MonoBehaviour {
 
-    NavMeshScript find;
-	// Use this for initialization
+    public NavMeshScript find;
+    private int i = 0;
+    
 	void Start () {
-        find = gameObject.GetComponent<NavMeshScript>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
 
     }
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Enter");
         find.find = !find.find;
+        i=0;
+    }
+
+    void OnTriggerStay(){
+        i++;
+        if (i>100){
+            find.find = !find.find;
+            i=0;
+        }
+    }
+
+    void OnTriggerExit(){
+        i=0;
     }
 }
