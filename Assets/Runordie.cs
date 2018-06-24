ff158f99f9a2af68914a3e0436f0abcdee5f8ea0
Otlet <otlet@otlet.pl>
2018-06-24 11:37:32 +0200

    Initial commit

---
+++ Assets/Runordie.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Runordie : MonoBehaviour {
+
+    public Button clicker;
+    public Animator leftHand;
+    public Animator rightHand;
+    public Animator body;
+    public Animator leftLeg;
+    public Animator rightLeg;
+    // Use this for initialization
+    void Start ()
+    {
+        clicker.onClick.AddListener(TaskOnClick);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+        leftHand.SetBool("Run", !leftHand.GetBool("Run"));
+        rightHand.SetBool("Run", !rightHand.GetBool("Run"));
+        body.SetBool("Run", !body.GetBool("Run"));
+        leftLeg.SetBool("Run", !leftLeg.GetBool("Run"));
+        rightLeg.SetBool("Run", !rightLeg.GetBool("Run"));
+    }
+}
