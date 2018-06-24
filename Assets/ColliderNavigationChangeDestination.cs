240729b18e25f6716b701b5de6e6fff192d7a3ea
Otlet <otlet@otlet.pl>
2018-06-24 14:05:29 +0200

    Koniec przerwy

---
+++ Assets/ColliderNavigationChangeDestination.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderNavigationChangeDestination : MonoBehaviour {
+
+    NavMeshScript find;
+	// Use this for initialization
+	void Start () {
+        find = gameObject.GetComponent<NavMeshScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter");
+        find.find = !find.find;
+    }
+}
---
+++ Assets/NavMeshScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshScript : MonoBehaviour {
+
+    NavMeshAgent agent;
+    public Transform target;
+    public Transform target2;
+    public bool find = true;
+
+	// Use this for initialization
+	void Start () {
+        agent = transform.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (find)
+            agent.SetDestination(target.position);
+        else
+            agent.SetDestination(target2.position);
+	}
+}
