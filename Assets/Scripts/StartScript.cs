cc0293b4e67ea561bd7ed3a878b036e2dcc9d09d
otlet <otlet@otlet.pl>
2018-06-25 18:34:21 +0200

    Add something

---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartScript : MonoBehaviour {
+
+	public Button startButton;
+	public Button exitButton;
+	// Use this for initialization
+	void Start () {
+		startButton.onClick.AddListener(startGame);
+		exitButton.onClick.AddListener(exitGame);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void startGame() {
+		Debug.Log("Click");
+	}
+
+	void exitGame() {
+		Application.Quit();
+	}
+}
